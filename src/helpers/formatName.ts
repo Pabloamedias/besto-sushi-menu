32f0f534479f52ced6eaa6223b2f8f47ae2ebf1d
Pabloamedias <p.olmedosaez@gmail.com>
2024-11-20 00:43:39 -0300

    simplify format name

--- src/helpers/formatName.ts
+++ src/helpers/formatName.ts
@@ -1,12 +1,8 @@
 export const formatHelper = (nombre: string): string => {
   return nombre
-    .split("-") // Divide el nombre en palabras usando "-" como delimitador
+    .split(" ") // Divide el nombre en palabras usando " " como delimitador
     .map(
-      (word) => word.replace(/\d+/g, "") // Elimina los números de cada palabra
+      (word) => word.toLowerCase() // Convierte la primera letra a mayúscula y el resto a minúscula
     )
-    .map(
-      (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() // Convierte la primera letra a mayúscula y el resto a minúscula
-    )
-    .filter((word) => word.length > 0) // Elimina palabras vacías que podrían resultar de la eliminación de números
-    .join(" "); // Une las palabras con espacios en blanco
+    .join("-"); // Une las palabras con espacios en blanco
 };
